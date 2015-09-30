53eb7469f95972071fd5af7c8ac754fd51e0c3a4
MrTj458 <thod0127@740s-pl-122.local>
2015-09-30 11:25:18 -0600

    Added more documentation

--- src/input/controller/PopupRunner.java
+++ src/input/controller/PopupRunner.java
@@ -1,5 +1,9 @@
 package input.controller;
 
+/**
+ * Ye olde Runner. Doesen't do much. Just makes the whole program work.
+ * @author thod0127
+ */
 public class PopupRunner
 {
 	public static void main (String [] args)
